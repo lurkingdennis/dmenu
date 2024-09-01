deacc0fce56ee6c401b32ba078040813d89d3595
Dennis C. <dpclements@proton.me>
2024-09-01 12:22:25 -0700

    matched font size with dwm

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const float menu_height_ratio = 2.0f;  /* This is the ratio used in the o
 static const int user_bh = 8;               /* add an defined amount of pixels to the bar height */
 
 static const char *fonts[] = {
-	"terminess nerd font:size=10"
+	"terminess nerd font:size=9"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
